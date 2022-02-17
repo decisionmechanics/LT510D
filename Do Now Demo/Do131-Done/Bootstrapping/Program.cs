ec222aff45ff97e7f9f7def983d4021a260a3f89
Andrew Tait <Andrew.Tait@decisionmechanics.com>
2022-02-17 19:38:46 +0000

    Add P/Invoke example

---
+++ Do Now Demo/Do131-Done/Bootstrapping/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+var data = Enumerable.Range(1, 10).ToArray();
+
+BootstrapMedians(data, (UIntPtr)data.Length, 10000, out double mu, out double sigma);
+
+Console.WriteLine($"Median sampling distribution: mean = {mu:N2}, standard deviation = {sigma:N2}");
+
+[DllImport("bootstrapper.dll", EntryPoint  = "bootstrap_medians", CharSet = CharSet.Auto)] 
+static extern void BootstrapMedians([In] int[] data, UIntPtr size, uint iterations, out double mu, out double sigma);
